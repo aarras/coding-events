c932ae3d27a859560bbc550eac41a785da16c5fb
Adam Arras <adamarras@gmail.com>
2021-03-14 17:59:08 -0500

    17.5 Exercise Complete

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,41 @@
+package org.launchcode.codingevents.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class EventCategory {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Size(min=3, max= 20)
+    private String name;
+
+    public EventCategory(String name) {
+        this.name = name;
+    }
+
+    public EventCategory() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
